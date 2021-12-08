05fab430ca6744c86413373b0a760bc233b32ca4
yangzhengca <yangzhengca>
2021-12-08 11:56:36 -0600

    Seprated GraphQL schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
-const db = require('./db');
 
+// Local mudule imports
+// Import MongoDB settings
+const db = require('./db');
+// Import MongoDB schema
 const models = require('./models');
+// Import GraphQL schema
+const typeDefs = require('./schema');
+// Import GraphQL resolvers
+const resolvers = require('./resolvers');
 
 const app = express();
 
@@ -13,62 +19,19 @@ const PORT = process.env.PORT || 5000
 // Set mongodb url
 const DB_HOST = process.env.DB_HOST;
 
-// Temp data
-let notes = [
-    { id: '1', content: "This is a note", author: 'Adam Scott' },
-    { id: '2', content: "This is another note", author: 'Harlow Everly' },
-    { id: '3', content: "Oh hey look, another note", author: 'Riley Harrison' }
-];
-
-
-
-// Construct a schema, using GraphQL's schema language
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-
-    type Query {
-        hello: String!
-        notes: [Note!]!
-        note(id: ID!):Note!
-
-    }
-
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`;
-
-// Provide resolver function to our schema
-const resolvers = {
-    Query: {
-        hello: () => 'Hello World!',
-        notes: async () => {
-            return await models.Note.find();
-        },
-        note: async (parent, args) => {
-            return await models.Note.findById(args.id);
-        }
-    },
-
-    Mutation: {
-        newNote: async (parent, args) => {
-            return await models.Note.create({
-                content: args.content,
-                author: 'Adam Scott'
-            })
-        }
-    }
-};
 
 // Connect to database
 db.connect(DB_HOST);
 
 // Setup ApolloServer
-const server = new ApolloServer({typeDefs, resolvers});
+const server = new ApolloServer({
+    typeDefs, 
+    resolvers,
+    context: () => {
+        // Add the db models to the context
+        return { models };
+    }
+});
 
 // Apply the Apollo GraphQL middleware and set path to /api
 server.applyMiddleware({app, path: '/api'});
